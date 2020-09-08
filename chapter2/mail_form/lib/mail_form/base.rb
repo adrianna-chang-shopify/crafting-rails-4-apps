7dbb30f9fb7658e58f9cb2f44446b4f30fddbe0e
Adrianna Chang <adrianna.chang@shopify.com>
2020-09-08 15:17:20 -0400

    Make MailForm::Base Active Model API compliant

--- chapter2/mail_form/lib/mail_form/base.rb
+++ chapter2/mail_form/lib/mail_form/base.rb
@@ -1,12 +1,23 @@
 module MailForm
   class Base
-    include ActiveModel::AttributeMethods # 1) attribute methods behavior
+    include ActiveModel::AttributeMethods
+    include ActiveModel::Conversion
+    include ActiveModel::Validations
+    extend ActiveModel::Naming
+    extend ActiveModel::Translation
+
     attribute_method_prefix 'clear_'
     attribute_method_suffix '?'
 
     def self.attributes(*names)
       attr_accessor(*names)
       define_attribute_methods(names)
+    end
+
+    # An object is persisted if it's neither a new record, nor destroyed
+    # Since we're not saving objects to the db, #persisted? should always return false
+    def persisted?
+      false
     end
 
     protected
