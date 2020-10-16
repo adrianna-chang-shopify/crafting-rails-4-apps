d4aec37c4b71787795a16c617252ebabd56b1930
Adrianna Chang <adrianna.chang@shopify.com>
2020-10-16 08:42:26 -0400

    Use templater to create CMS

--- chapter3/templater/config/routes.rb
+++ chapter3/templater/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users
   resources :sql_templates
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "cms/*page", to: "cms#respond"
 end
