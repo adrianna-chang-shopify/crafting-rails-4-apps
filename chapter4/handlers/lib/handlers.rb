396612481c59373aeb2eca5b02bcfa101ac81621
Adrianna Chang <adrianna.chang@shopify.com>
2020-10-28 10:33:58 -0400

    Create string template handler

--- chapter4/handlers/lib/handlers.rb
+++ chapter4/handlers/lib/handlers.rb
@@ -2,6 +2,7 @@ require "handlers/railtie"
 require "action_view/template"
 
 ActionView::Template.register_template_handler :rb, lambda { |_, source| source }
+ActionView::Template.register_template_handler :string, lambda { |_, source| "%Q{#{source}}" }
 
 module Handlers
   # Your code goes here...
--- chapter4/handlers/test/dummy/config/routes.rb
+++ chapter4/handlers/test/dummy/config/routes.rb
@@ -1,3 +1,4 @@
 Dummy::Application.routes.draw do
   get "/handlers/rb_handler", to: "handlers#rb_handler"
+  get "/handlers/string_handler", to: "handlers#string_handler"
 end
